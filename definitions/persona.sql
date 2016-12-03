614a0cdb08e72f51a7f13e8de9ae04012ba51ccc
Sergio Garcia <sergio@garciparedes.me>
2016-12-03 19:54:08 +0100

    Done query 8

--- definitions/persona.sql
+++ definitions/persona.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Persona (
     pid INTEGER,
     nombre VARCHAR(140),
-    f_nacim TIMESTAMP,
-    f_alta TIMESTAMP,
+    f_nacim TIME,
+    f_alta TIME,
 
     PRIMARY KEY (pid)
 );
--- queries/query-08.sql
+++ queries/query-08.sql
@@ -2,3 +2,21 @@
 Encontrar a los dos personas que hayan nacido con fechas más cercanas (sin
 contar las que han nacido en la misma fecha)
  */
+
+SELECT
+    p1.nombre,
+    p2.nombre,
+    p1.f_nacim - p2.f_nacim
+FROM
+    persona p1,
+    persona p2
+WHERE p1.f_nacim - p2.f_nacim = (
+    SELECT min(p1.f_nacim - p2.f_nacim)
+    FROM
+        persona p1,
+        persona p2
+    WHERE
+        p1.pid <> p2.pid AND
+        p1.f_nacim <> p2.f_nacim AND
+        p1.f_nacim > p2.f_nacim
+);
