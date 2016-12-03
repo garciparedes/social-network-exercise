dff57d0878c7139e3ca62ae79ac6330d0a90f89e
Sergio Garcia <sergio@garciparedes.me>
2016-12-03 15:36:54 +0100

    Worked on ranking queries

--- queries/query-02.sql
+++ queries/query-02.sql
@@ -2,22 +2,27 @@
 Seleccionar a la(s) persona(s) con más número de amigos
  */
 
+
+WITH CuentaAmigos AS (
+    SELECT
+        p1.nombre,
+        COUNT(*) AS cuenta
+    FROM
+        persona p1,
+        amistad a1
+    WHERE
+        p1.pid = a1.pid1
+    GROUP BY
+        p1.nombre
+)
 SELECT
-    p1.nombre,
-    COUNT(*) AS cuenta
+    CA1.nombre,
+    CA1.cuenta
 FROM
-    persona p1,
-    amistad a1
-WHERE
-    p1.pid = a1.pid1
-GROUP BY p1.nombre
-HAVING COUNT(*) >= ALL (
-        SELECT
-            count(*) as cuenta2
-        FROM
-            persona p2,
-            amistad a2
-        WHERE
-            p2.pid = a2.pid1
-        GROUP BY p2.nombre
-    )
+    CuentaAmigos CA1
+WHERE CA1.cuenta >= ALL (
+    SELECT CA2.cuenta
+    FROM
+        CuentaAmigos CA2
+)
+
--- queries/query-06.sql
+++ queries/query-06.sql
@@ -2,14 +2,30 @@
 Encontrar a las 5 personas con mayor numero de amigos
  */
 
+WITH CuentaAmigos AS (
+    SELECT
+        p1.nombre,
+        COUNT(*) AS cuenta
+    FROM
+        persona p1,
+        amistad a1
+    WHERE
+        p1.pid = a1.pid1
+    GROUP BY
+        p1.nombre
+)
 SELECT
-    p1.nombre,
-    COUNT(*) AS cuenta
+    CA1.nombre,
+    CA1.cuenta
 FROM
-    persona p1,
-    amistad a1
-WHERE
-    p1.pid = a1.pid1
-GROUP BY p1.nombre
-ORDER BY cuenta DESC
-LIMIT 5
+    CuentaAmigos CA1
+WHERE 5 >= (
+    SELECT
+        COUNT(*)
+    FROM
+        CuentaAmigos CA2
+    WHERE CA2.cuenta > CA1.cuenta
+)
+ORDER BY CA1.cuenta DESC
+
+
